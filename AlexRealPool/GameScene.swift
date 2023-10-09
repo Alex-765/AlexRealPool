c407ce5c9cbc67494a17b2f5c726c33ae00b4ba0
Alex Bater <bater.a@etoncollege.org.uk>
2023-10-09 13:16:58 +0100

    Dodgy Physics

--- AlexRealPool/GameScene.swift
+++ AlexRealPool/GameScene.swift
@@ -9,9 +9,9 @@
 import SpriteKit
 
 class Ball: SKSpriteNode {
-    // Initialize the Ball with a size of 5x5
+    // Initialize the Ball with a size of 20x20
     init(texture: SKTexture?) {
-        let size = CGSize(width: 22, height: 22) // Set the size to 5x5
+        let size = CGSize(width: 20, height: 20)
         
         super.init(texture: texture, color: .clear, size: size)
         self.zPosition = 1
@@ -19,6 +19,7 @@ class Ball: SKSpriteNode {
         self.physicsBody = SKPhysicsBody(circleOfRadius: 10)
         self.physicsBody?.usesPreciseCollisionDetection = true
         self.physicsBody?.restitution = 1
+        self.physicsBody?.collisionBitMask = 0b0001
 //        self.physicsBody?.mass = 1
     }
     
@@ -27,10 +28,30 @@ class Ball: SKSpriteNode {
     }
 }
 
+//class Wall: SKPhysicsBody {
+//    init(edgeFrom p1: CGPoint,
+//         to p2: CGPoint){
+//        
+//        let point1 = CGPoint(x:800, y:100)
+//        let point2 = CGPoint(x:800, y:10)
+//        super.init(edgeFrom: point1, to: point2)
+//        
+//    }
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("Error")
+//    }
+//}
+
 
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody?.usesPreciseCollisionDetection = true
+        self.physicsBody?.restitution = 1.0
+        self.physicsBody?.friction = 0.2
+        
+//        About Collisions and contacts
         
         // Table set up
         
@@ -95,7 +116,7 @@ class GameScene: SKScene {
         addChild(blackBall)
         
         cueBall.physicsBody?.velocity = self.physicsBody!.velocity
-        cueBall.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 0))
+        cueBall.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 0.1))
         
 //       Main Menu, Images
 //       Creation of Wall
