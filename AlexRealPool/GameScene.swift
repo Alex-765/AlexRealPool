6f478e6cdc0bee81dd444e858ff266750b3b33d4
Alex Bater <bater.a@etoncollege.org.uk>
2023-11-09 09:54:45 +0000

    Committ

--- AlexRealPool/GameScene.swift
+++ AlexRealPool/GameScene.swift
@@ -11,16 +11,17 @@ import SpriteKit
 class Ball: SKSpriteNode {
     // Initialize the Ball with a size of 20x20
     init(texture: SKTexture?) {
-        let size = CGSize(width: 20, height: 20)
+        let size = CGSize(width:20, height: 20)
         
         super.init(texture: texture, color: .clear, size: size)
         self.zPosition = 1
         
         self.physicsBody = SKPhysicsBody(circleOfRadius: 10)
-        self.physicsBody?.usesPreciseCollisionDetection = true
-        self.physicsBody?.restitution = 0.8
+//        self.physicsBody?.usesPreciseCollisionDetection = true
+        self.physicsBody?.restitution = 1
         self.physicsBody?.collisionBitMask = 0b0001
-        self.physicsBody?.linearDamping = 0.05
+        self.physicsBody?.linearDamping = 0.1
+        self.physicsBody?.allowsRotation = false
     }
     
     required init?(coder: NSCoder) {
@@ -31,25 +32,42 @@ class Ball: SKSpriteNode {
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        let point1 = CGPoint(x:370, y:200)
-        let point2 = CGPoint(x:370, y:-200)
-        let point3 = CGPoint(x:-370, y: -200)
-        let point4 = CGPoint(x:370, y: -200)
         
+        let point1 = CGPoint(x:380, y:140)
+        let point2 = CGPoint(x:380, y:-140)
+        let point3 = CGPoint(x:-380, y: 140)
+        let point4 = CGPoint(x:-380, y: -140)
+
         
-        let wall1 = SKSpriteNode()
+        let wall1 = SKNode()
         wall1.physicsBody = SKPhysicsBody(edgeFrom: point1, to: point2)
-        wall1.physicsBody?.usesPreciseCollisionDetection = true
-        wall1.physicsBody?.restitution = 0.8
+//        wall1.physicsBody?.usesPreciseCollisionDetection = true
+        wall1.physicsBody?.restitution = 1
         wall1.physicsBody?.collisionBitMask = 0b0001
+        addChild(wall1)
 
         
-        let wall2 = SKSpriteNode()
-        wall2.physicsBody = SKPhysicsBody(edgeFrom: point3, to: point4)
-        wall2.physicsBody?.usesPreciseCollisionDetection = true
-        wall2.physicsBody?.restitution = 0.8
+        let wall2 = SKNode()
+        wall2.physicsBody = SKPhysicsBody(edgeFrom: point2, to: point4)
+//        wall2.physicsBody?.usesPreciseCollisionDetection = true
+        wall2.physicsBody?.restitution = 1
         wall2.physicsBody?.collisionBitMask = 0b0001
-
+        addChild(wall2)
+        
+        let wall3 = SKNode()
+        wall3.physicsBody = SKPhysicsBody(edgeFrom: point1, to: point3)
+//        wall3.physicsBody?.usesPreciseCollisionDetection = true
+        wall3.physicsBody?.restitution = 1
+        wall3.physicsBody?.collisionBitMask = 0b0001
+        addChild(wall3)
+        
+        let wall4 = SKNode()
+        wall4.physicsBody = SKPhysicsBody(edgeFrom: point3, to: point4)
+//        wall4.physicsBody?.usesPreciseCollisionDetection = true
+        wall4.physicsBody?.restitution = 1
+        wall4.physicsBody?.collisionBitMask = 0b0001
+        addChild(wall4)
+        
     
 
         // Table set up
@@ -65,10 +83,10 @@ class GameScene: SKScene {
         
         // Create Red and Blue Ball Textures
 
-        let redBallTexture = SKTexture(imageNamed: "RedBall")
+        let redBallTexture = SKTexture(imageNamed: "BlueBall")
         let blueBallTexture = SKTexture(imageNamed: "BlueBall")
         let cueBallTexture = SKTexture(imageNamed: "RedBall")
-        let blackBallTexture = SKTexture(imageNamed: "RedBall")
+        let blackBallTexture = SKTexture(imageNamed: "BlueBall")
 
         
         // Creating Break Positions
@@ -100,11 +118,19 @@ class GameScene: SKScene {
             blueBall.position = CGPoint(x: (frame.midX + x), y:(frame.midY + y))
             addChild(blueBall)
         }
+//        let blueBall = Ball(texture: blueBallTexture)
+//        blueBall.position = point1
+//        addChild(blueBall)
+        
+        
         for (x,y) in redBallPositions {
             let redBall = Ball(texture: redBallTexture)
             redBall.position = CGPoint(x: (frame.midX + x), y:(frame.midY + y))
             addChild(redBall)
         }
+//        let redBall = Ball(texture: redBallTexture)
+//        redBall.position = point2
+//        addChild(redBall)
         
         let cueBall = Ball(texture: cueBallTexture)
         cueBall.position = CGPoint(x: (frame.midX - 195), y:(frame.midY))
