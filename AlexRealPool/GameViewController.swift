38495b40636258331a40243c579bbf932bf780b6
Alex <a.b@c.com>
2024-01-31 17:56:05 +0000

    g

--- AlexRealPool/GameViewController.swift
+++ AlexRealPool/GameViewController.swift
@@ -15,7 +15,7 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            if let scene = SKScene(fileNamed: "MenuScene") {
+            if let scene = SKScene(fileNamed: " genuScene") {
                 scene.scaleMode = .resizeFill
                 view.presentScene(scene)
             }
