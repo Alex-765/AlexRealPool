5a7af887cc5a1f49311ef6834ca20da7c4cc6360
alexanderbater <bater.a@etoncollege.org.uk>
2024-01-31 18:07:17 +0000

    pop

--- AlexRealPool/MenuScene.swift
+++ AlexRealPool/MenuScene.swift
@@ -14,17 +14,25 @@ class MenuScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        let background = SKSpriteNode(color: .cyan, size: view.frame.size)
+        let background = SKSpriteNode(color: .white, size: view.frame.size)
         background.position = CGPoint(x: frame.midX, y: frame.midY)
-        background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         background.zPosition = -1
         addChild(background)
+        
+        let title = SKLabelNode(fontNamed: "Impact")
+        title.text = "Real Pool"
+        title.position = CGPoint(x: frame.midX, y: frame.midY + 30)
+        title.fontColor = .black
+        title.fontSize = 70
+        title.zPosition = 1
+        addChild(title)
+        
 
         playButton = SKLabelNode(fontNamed: "Impact")
         playButton.text = "Play Game"
-        playButton.position = CGPoint(x: frame.midX, y: frame.midY)
+        playButton.position = CGPoint(x: frame.midX, y: frame.midY - 100)
         playButton.fontColor = .red
-        playButton.fontSize = 50
+        playButton.fontSize = 30
         playButton.zPosition = 1
         
         addChild(playButton)
